b29126389b9acc47ba64deb335de0edcda9d94e2
wischli <w.freude@icloud.com>
2022-01-22 08:52:39 +0100

    fix: faucet, delegation

--- fillDelegations.js
+++ fillDelegations.js
@@ -5,7 +5,8 @@ const { cryptoWaitReady, mnemonicGenerate } = require('@polkadot/util-crypto');
 require('dotenv').config();
 
 // Initialise the provider to connect to the local node
-const provider = process.env.WS_ADDRESS || new WsProvider('wss://peregrine-stg.kilt.io/para');
+const wsAddress = process.env.WS_ADDRESS || 'wss://peregrine-stg.kilt.io/para';
+const provider = new WsProvider('wss://peregrine-stg.kilt.io/para');
 const SUDO = process.env.SUDO;
 
 // Set up an account from its private key.
@@ -14,9 +15,14 @@ function initAccount(privKey) {
     return keyring.addFromUri(privKey);
 }
 
+// Format balance into KILT by applying deenomination
+function formatBalance(amount) {
+    return Number.parseInt(amount) / Math.pow(10, 15);
+}
+
 // Faucet balance to target
-async function faucet(sudoAcc, amount, target, api) {
-    const tx = api.tx.sudo.sudo(api.tx.balances.setBalance(target, amount.toString(), '0'));
+async function faucet({ sudoAcc, faucetAmount, target, api }) {
+    const tx = api.tx.sudo.sudo(api.tx.balances.setBalance(target, faucetAmount.toString(), '0'));
 
     await new Promise(async (resolve, reject) => {
         const unsub = await tx.signAndSend(sudoAcc, ({ status, events }) => {
@@ -35,78 +41,111 @@ async function faucet(sudoAcc, amount, target, api) {
                                 // for module errors, we have the section indexed, lookup
                                 const decoded = api.registry.findMetaError(error.asModule);
                                 const { docs, name, section } = decoded;
-    
+
                                 console.log(`${section}.${name}: ${docs.join(' ')}`);
                             } else {
                                 // Other, CannotLookup, BadOrigin, no extra info
                                 console.log(error.toString());
                             }
+                            console.log(`Rejecting due to sudo error`);
                             reject();
                         }
                     });
-                    if (status.isFinalized) {
-                        console.log("unsubbing")
-                        unsub();
-                        resolve();
-                    }
+                if (status.isFinalized) {
+                    unsub();
+                    resolve();
+                }
             }
         });
     })
 
     const {
         data: { free },
-      } = await api.query.system.account(target);
+    } = await api.query.system.account(target);
     if (!free) {
         throw Error(`Delegator ${target} has not received funds`);
     }
-    console.log(`\t Received funds: ${Number.parseInt(free) / Math.pow(10, 15)} KILT`);
+    console.log(`\t Target successfully received funds: ${formatBalance(free)} KILT`);
+}
+
+async function delegate({ delegator, delegationAmount, collatorAddress, api }) {
+    const tx = api.tx.parachainStaking.joinDelegators(collatorAddress, delegationAmount);
+
+    await new Promise(async (resolve, reject) => {
+        const unsub = await tx.signAndSend(delegator, ({ status, events }) => {
+            if (status.isInBlock || status.isFinalized) {
+                events
+                    // find/filter for failed events
+                    .filter(({ event }) =>
+                        api.events.system.ExtrinsicFailed.is(event)
+                    )
+                    // we know that data for system.ExtrinsicFailed is
+                    // (DispatchError, DispatchInfo)
+                    .forEach(({ event: { data: [error, info] } }) => {
+                        if (error.isModule) {
+                            // for module errors, we have the section indexed, lookup
+                            const decoded = api.registry.findMetaError(error.asModule);
+                            const { docs, method, section } = decoded;
+
+                            console.log(`${section}.${method}: ${docs.join(' ')}`);
+                        } else {
+                            // Other, CannotLookup, BadOrigin, no extra info
+                            console.log(error.toString());
+                        }
+                        console.log(`Rejecting due to tx error`)
+                        reject();
+                    });
+                resolve();
+                if (status.isFinalized) {
+                    unsub();
+                    resolve();
+                }
+            }
+        })
+    });
+
+    console.log(`\t Successfully delegated`);
 }
 
 // Execute script
 async function main() {
     // Create our API with a default connection to the local node
     const api = await ApiPromise.create({ provider });
-    console.log(`Connected to ${provider}`);
+    console.log(`Connected to ${wsAddress}`);
     await cryptoWaitReady();
-    
+
     // Get constants
     const maxDelegatorsPerCollator = (await api.consts.parachainStaking.maxDelegatorsPerCollator).toNumber();
     const minDelegatorStake = new BN((await api.consts.parachainStaking.minDelegatorStake).toHuman(), 10);
-    
+
     // Get active collators
     const topCandidates = (await api.query.parachainStaking.topCandidates()).toJSON();
     const numCollators = (await api.query.parachainStaking.maxSelectedCandidates()).toNumber();
     const activeCollators = topCandidates.slice(0, numCollators).map(collator => collator.owner);
-    
+
     // Prep sudo
     const sudoAcc = initAccount(SUDO);
     console.log(`Initiating collator iteration`);
-    
+
     // Iterate all collators
-    let counter = 0;
+    let counter = 1;
     for (const collator of activeCollators) {
-        console.log(`${collator}: 1`)
         // Get active delegators
         const numActiveDelegators = (await api.query.parachainStaking.candidatePool(collator)).toHuman().delegators.length;
-        const missingDelegators = maxDelegatorsPerCollator - numActiveDelegators;
-        console.log(`[${counter}/${activeCollators.length}] Collator ${collator} is missing ${missingDelegators} many delegators...`);
-        
-        // Fill delegation slots
-        for (let i = 0; i < missingDelegators; i += 1) {
-            console.log(`\t[${i}/${missingDelegators}] Creating delegator and providing liquidity`);
-            let tmpDelegator = initAccount(mnemonicGenerate());
-            await faucet(sudoAcc, 1.5 * minDelegatorStake, tmpDelegator.address, api);
+        const numMissingDelegators = maxDelegatorsPerCollator - numActiveDelegators;
+        console.log(`[${counter}/${activeCollators.length}] Collator ${collator} is missing ${numMissingDelegators} delegators`);
 
-            console.log(`\t [${i}/${missingDelegators}] Delegate with ${tmpDelegator.address}`);
-            const unsub = await api.tx.parachainStaking.joinDelegators(collator, minDelegatorStake).signAndSend(tmpDelegator, ({ status }) => {
-                if (status.isInBlock) {
-                    console.log(`\t included in ${status.asInBlock}`);
-                }
-                if (status.isFinalized) {
-                    unsub();
-                }
-            });
+        // Fill delegation slots
+        for (let i = 1; i <= numMissingDelegators + 1; i += 1) {
+            console.log(`\t [${i}a/${numMissingDelegators}] Creating delegator and providing liquidity`);
+            let delegator = initAccount(mnemonicGenerate());
+            await faucet({ sudoAcc, faucetAmount: minDelegatorStake.mul(new BN(2, 10)), target: delegator.address, api });
+
+            console.log(`\t [${i}b/${numMissingDelegators}] Delegate with ${delegator.address}`);
+            await delegate({ delegator, delegationAmount: minDelegatorStake, collatorAddress: collator, api });
         }
+        counter += 1;
     }
+    console.log(`Done with filling delegator slots, disconnecting ${wsAddress}`)
 }
 main().catch(console.error).finally(() => process.exit());
