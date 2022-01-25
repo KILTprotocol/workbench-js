1d6daf5851471ea5ac065962d628d3ab7a9e418e
wischli <w.freude@icloud.com>
2022-01-25 14:40:50 +0100

    feat: use faucet instead of sudo

--- fillDelegations.js
+++ fillDelegations.js
@@ -6,8 +6,9 @@ require('dotenv').config();
 
 // Initialise the provider to connect to the local node
 const wsAddress = process.env.WS_ADDRESS || 'wss://peregrine-stg.kilt.io/para';
-const provider = new WsProvider('wss://peregrine-stg.kilt.io/para');
-const SUDO = process.env.SUDO;
+// const wsAddress = process.env.WS_ADDRESS || 'wss://peregrine.kilt.io/parachain-public-ws';
+const provider = new WsProvider(wsAddress);
+const FAUCET = process.env.FAUCET;
 
 // Set up an account from its private key.
 function initAccount(privKey) {
@@ -15,49 +16,49 @@ function initAccount(privKey) {
     return keyring.addFromUri(privKey);
 }
 
-// Format balance into KILT by applying deenomination
+// Format balance into KILT by applying denomination
 function formatBalance(amount) {
     return Number.parseInt(amount) / Math.pow(10, 15);
 }
 
+function txHandlerNormal({ resolve, reject, unsub, status, events, api, }) {
+    if (status.isInBlock || status.isFinalized) {
+        events
+            // find/filter for failed events
+            .filter(({ event }) =>
+                api.events.system.ExtrinsicFailed.is(event)
+            )
+            // we know that data for system.ExtrinsicFailed is
+            // (DispatchError, DispatchInfo)
+            .forEach(({ event: { data: [error, info] } }) => {
+                if (error.isModule) {
+                    // for module errors, we have the section indexed, lookup
+                    const decoded = api.registry.findMetaError(error.asModule);
+                    const { docs, method, section } = decoded;
+
+                    console.log(`${section}.${method}: ${docs.join(' ')}`);
+                } else {
+                    // Other, CannotLookup, BadOrigin, no extra info
+                    console.log(error.toString());
+                }
+                console.log(`Rejecting due to tx error`)
+                reject();
+            });
+        resolve();
+        if (status.isFinalized) {
+            unsub();
+            resolve();
+        }
+    }
+}
+
 // Faucet balance to target
-async function faucet({ sudoAcc, faucetAmount, target, api }) {
-    const tx = api.tx.sudo.sudo(api.tx.balances.setBalance(target, faucetAmount.toString(), '0'));
+async function faucetTransfer({ faucetAcc, faucetAmount, target, api }) {
+    const tx = api.tx.balances.transfer(target, faucetAmount.toString());
 
     await new Promise(async (resolve, reject) => {
-        const unsub = await tx.signAndSend(sudoAcc, ({ status, events }) => {
-            if (status.isInBlock || status.isFinalized) {
-                events
-                    // We know this tx should result in `Sudid` event.
-                    .filter(({ event }) =>
-                        api.events.sudo.Sudid.is(event)
-                    )
-                    // We know that `Sudid` returns just a `Result`
-                    .forEach(({ event: { data: [result] } }) => {
-                        // Now we look to see if the extrinsic was actually successful or not...
-                        if (result.isError) {
-                            let error = result.asError;
-                            if (error.isModule) {
-                                // for module errors, we have the section indexed, lookup
-                                const decoded = api.registry.findMetaError(error.asModule);
-                                const { docs, name, section } = decoded;
-
-                                console.log(`${section}.${name}: ${docs.join(' ')}`);
-                            } else {
-                                // Other, CannotLookup, BadOrigin, no extra info
-                                console.log(error.toString());
-                            }
-                            console.log(`Rejecting due to sudo error`);
-                            reject();
-                        }
-                    });
-                if (status.isFinalized) {
-                    unsub();
-                    resolve();
-                }
-            }
-        });
-    })
+        const unsub = await tx.signAndSend(faucetAcc, ({ status, events }) => txHandlerNormal({ resolve, reject, unsub, status, events, api }));
+    });
 
     const {
         data: { free },
@@ -72,36 +73,7 @@ async function delegate({ delegator, delegationAmount, collatorAddress, api }) {
     const tx = api.tx.parachainStaking.joinDelegators(collatorAddress, delegationAmount);
 
     await new Promise(async (resolve, reject) => {
-        const unsub = await tx.signAndSend(delegator, ({ status, events }) => {
-            if (status.isInBlock || status.isFinalized) {
-                events
-                    // find/filter for failed events
-                    .filter(({ event }) =>
-                        api.events.system.ExtrinsicFailed.is(event)
-                    )
-                    // we know that data for system.ExtrinsicFailed is
-                    // (DispatchError, DispatchInfo)
-                    .forEach(({ event: { data: [error, info] } }) => {
-                        if (error.isModule) {
-                            // for module errors, we have the section indexed, lookup
-                            const decoded = api.registry.findMetaError(error.asModule);
-                            const { docs, method, section } = decoded;
-
-                            console.log(`${section}.${method}: ${docs.join(' ')}`);
-                        } else {
-                            // Other, CannotLookup, BadOrigin, no extra info
-                            console.log(error.toString());
-                        }
-                        console.log(`Rejecting due to tx error`)
-                        reject();
-                    });
-                resolve();
-                if (status.isFinalized) {
-                    unsub();
-                    resolve();
-                }
-            }
-        })
+        const unsub = await tx.signAndSend(delegator, ({ status, events }) => txHandlerNormal({ resolve, reject, unsub, status, events, api }))
     });
 
     console.log(`\t Successfully delegated`);
@@ -116,7 +88,7 @@ async function main() {
 
     // Get constants
     const maxDelegatorsPerCollator = (await api.consts.parachainStaking.maxDelegatorsPerCollator).toNumber();
-    const minDelegatorStake = new BN((await api.consts.parachainStaking.minDelegatorStake).toHuman(), 10);
+    const minDelegatorStake = new BN((await api.consts.parachainStaking.minDelegatorStake).toString());
 
     // Get active collators
     const topCandidates = (await api.query.parachainStaking.topCandidates()).toJSON();
@@ -124,7 +96,7 @@ async function main() {
     const activeCollators = topCandidates.slice(0, numCollators).map(collator => collator.owner);
 
     // Prep sudo
-    const sudoAcc = initAccount(SUDO);
+    const faucetAcc = initAccount(FAUCET);
     console.log(`Initiating collator iteration`);
 
     // Iterate all collators
@@ -136,10 +108,11 @@ async function main() {
         console.log(`[${counter}/${activeCollators.length}] Collator ${collator} is missing ${numMissingDelegators} delegators`);
 
         // Fill delegation slots
-        for (let i = 1; i <= numMissingDelegators + 1; i += 1) {
+        for (let i = 1; i <= numMissingDelegators; i += 1) {
             console.log(`\t [${i}a/${numMissingDelegators}] Creating delegator and providing liquidity`);
+            
             let delegator = initAccount(mnemonicGenerate());
-            await faucet({ sudoAcc, faucetAmount: minDelegatorStake.mul(new BN(2, 10)), target: delegator.address, api });
+            await faucetTransfer({ faucetAcc, faucetAmount: minDelegatorStake.mul(new BN(2)), target: delegator.address, api });
 
             console.log(`\t [${i}b/${numMissingDelegators}] Delegate with ${delegator.address}`);
             await delegate({ delegator, delegationAmount: minDelegatorStake, collatorAddress: collator, api });
